191213332344d8b4b3b9933399f0c15919bbd238
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-04-16 14:27:52 +0200

    Adds letters for music avatar

--- components/MaterialAvatar.js
+++ components/MaterialAvatar.js
@@ -3,12 +3,31 @@ import { StyleSheet } from 'react-native';
 import { Avatar } from 'react-native-elements';
 
 const MaterialAvatar = ({ material }) => {
+  const getTypeLetter = (type) => {
+    if (type.trim() === '') return '???';
+
+    if (type.toLowerCase().includes('solo')) return 'Solo';
+    if (type.toLowerCase().includes('duet')) return 'Duet';
+    if (type.toLowerCase().includes('trio')) return 'Trio';
+    if (type.toLowerCase().includes('quartet')) return 'Quart';
+    if (type.toLowerCase().includes('selection')) return 'Selec';
+    if (type.toLowerCase().includes('fanfare')) return 'Fanf.';
+    if (type.toLowerCase().includes('march')) return 'March';
+    if (type.toLowerCase().includes('song')) return 'Song';
+    if (type.toLowerCase().includes('medley')) return 'Medl.';
+    if (type.toLowerCase().includes('tone poem')) return 'T.P.';
+    if (type.toLowerCase().includes('overture')) return 'Overt.';
+    if (type.toLowerCase().includes('hymn')) return 'Hymn';
+
+    return type[0];
+  };
   return (
     <Avatar
       overlayContainerStyle={{ backgroundColor: '#1a3b5f' }}
-      material
       size='medium'
-      title={material.type[0]}
+      title={getTypeLetter(material.type)}
+      titleStyle={{ fontSize: '14px' }}
+      icon={{ name: 'music', type: 'font-awesome' }}
     />
   );
 };
